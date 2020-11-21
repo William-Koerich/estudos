7a0ffbd24f5c0f76daa32c3087ece12447bd7492
William Koerich <william.koerich@fw7.com.br>
2020-11-21 17:39:39 -0300

    Middleware.

---
+++ firstApi/node/middlewares.js
@@ -0,0 +1,31 @@
+/**
+ * Middleware pattern (chain of responsibility)
+ */
+
+const passo1 = (ctx, next) => {
+  ctx.valor = 'mid1'
+  next()
+}
+
+const passo2 = (ctx, next) => {
+  ctx.valor2 = 'mid2'
+  next()
+}
+
+const passo3 = ctx => (ctx.valor3 = 'mid3')
+
+const exec = (ctx, ...middlewares) => {
+  /**
+   * Função recursiva.
+   */
+  const execPasso = indice => {
+    middlewares &&
+      indice < middlewares.length &&
+      middlewares[indice](ctx, () => execPasso(indice + 1))
+  }
+  execPasso(0)
+}
+
+const ctx = {}
+exec(ctx, passo3, passo1, passo2)
+console.log(ctx)
